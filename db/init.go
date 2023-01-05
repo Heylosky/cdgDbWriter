1e2599b968cffe3b542350ba0261be3e4d8bc347
Heylosky <2547226479@qq.com>
2023-01-05 11:17:59 +0800

    first commit

---
+++ db/init.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"encoding/json"
+	"github.com/cdgProcessor/dbWriter/models"
+	"go.uber.org/zap"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func Writer(ch <-chan []byte) {
+	zap.L().Info("Inbound DB connection start")
+	dsn := "root:Welcome@1@tcp(172.25.240.10:30306)/mvp?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		zap.L().Error("open mysql failed, err: " + err.Error())
+	}
+
+	// 初始化数据库
+	db.AutoMigrate(&models.SMS{})
+
+	var message models.SMS
+
+	for {
+		sms := <-ch
+		json.Unmarshal(sms, &message)
+		db.Create(&message)
+	}
+}
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/cdgProcessor/dbWriter/db"
+	"github.com/cdgProcessor/dbWriter/logger"
+	"github.com/cdgProcessor/dbWriter/messageQ"
+	"go.uber.org/zap"
+)
+
+func main() {
+	logger.InitLogger("./dbWriter.log")
+	zap.L().Info("Processor sms to DB starting...")
+
+	in2dbChan := make(chan []byte)
+
+	go messageQ.MQRead(in2dbChan, "inboundSMS", "smsToDB", "consumeToDB")
+
+	db.Writer(in2dbChan)
+}
